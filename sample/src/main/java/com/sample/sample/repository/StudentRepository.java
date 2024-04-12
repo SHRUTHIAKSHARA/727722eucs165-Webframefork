e828aa58ae5a60e27f7f1197f19b40d4b8d6eddd
SHRUTHIAKSHARA <124983278+SHRUTHIAKSHARA@users.noreply.github.com>
2024-04-12 14:17:20 +0530

    Delete sample directory

--- sample/src/main/java/com/sample/sample/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sample.sample.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sample.sample.model.Student;
-
-public interface StudentRepository extends JpaRepository<Student,Integer> {
-    
-}
